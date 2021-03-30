d99290fa3bfbf80cc42b580ddc93f362d789b3fb
hol41 <hol41@berkeley.edu>
2021-03-29 23:35:53 -0700

    finished lab 3

---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlikTest {
+    @Test
+    public void test_Flik(){
+        assertTrue(Flik.isSameNumber(128, 128));
+    }
+}
